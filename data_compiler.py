a3a5368347b3afbb3c343d4cbc34b386318f096a
Ahsan <ahsan.moh426@gmail.com>
2026-02-17 19:38:50 -0500

    Third commit??? :(

---
+++ data_compiler.py
@@ -0,0 +1,11 @@
+"""
+Docstring for data_compiler
+In this section of my project, I'm going to use the statistics given by the several .csv files 
+I have uploaded to create new statistics, ones that are a bit more simplified and will help find the 
+standings and match results, without having to consult the large .csv file directly.
+
+The part where I need to use my brain the most is this next step. I'm going to be 
+
+when I finish up with this file, I'm going to export the needed data to the .json file called 
+new_stats, and then I can access that for my main project.
+"""
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Ok so lowkey here we have some premier league simulation thing here okay 
+here are the factors for winning and losing
+1. if you win you keep winning
+2. if you score goals, you win more often :)
+3. if you they score goals on you, you lose more often :(
+heres some stuff i might add:
+if you beat someone good, that says more about you than beating up spurs
+if you play at home, you are more likely to cook, and vice versa for away games
+include luck yk?
+maybe ref bias lol, cause i already have match reports with ref names
